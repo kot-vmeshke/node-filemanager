dd23bea06149e42bfd9b02f1759ef942aa75c4d4
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-10-12 15:39:51 +0200

    feat: set up command reception and exit options

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('Hello, File Manager!');
+import { stdin, cwd, chdir } from 'node:process';
+import os from 'node:os';
+
+const userName =
+  process.argv.find((item) => item.startsWith('--')).split('=')[1] ||
+  'UserName';
+
+chdir(os.homedir());
+
+stdin.on('data', (text) => {
+  const command = String(text).trim();
+
+  switch (command) {
+    case '.exit':
+      console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+      process.exit(0);
+    default:
+      console.log('Invalid input');
+      break;
+  }
+
+  console.log(`You are currently in ${cwd()}`);
+});
+
+process.on('SIGINT', () => {
+  console.log(`\nThank you for using File Manager, ${userName}, goodbye!`);
+  process.exit(0);
+});
+
+console.log(`Welcome to the File Manager, ${userName}!`);
+console.log(`You are currently in ${cwd()}`);
